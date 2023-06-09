a040f8729e20adb64c7e256122071cf6bc3eff11
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-06-08 20:33:09 -0600

    ReservationService, meodos para obtener los detalles de un vuelo y salvar una reservación

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -13,7 +13,15 @@ export class ReservationService {
 
   constructor(private http: HttpClient, private loginService: LoginService) { }
 
-  public getFlights(criteria : any){
+  public getFlights(criteria : any): any{
     return this.http.post(this.flightstUrl, criteria, this.loginService.httpOptions);
+  }
+
+  public getFlight(id : number): any{
+    return this.http.get(this.singleFlightUrl + id, this.loginService.httpOptions);
+  }
+
+  public saveReservation(reservation : any):any{
+    return this.http.post(this.saveReservationUrl, reservation, this.loginService.httpOptions);
   }
 }
