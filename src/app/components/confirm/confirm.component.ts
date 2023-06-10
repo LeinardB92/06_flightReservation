d4b9f4a42beb28f955e85309c66a781849543aa4
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-06-10 07:09:01 -0600

    implementación de ConfirmComponent

--- src/app/components/confirm/confirm.component.ts
+++ src/app/components/confirm/confirm.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-confirm',
   templateUrl: './confirm.component.html',
   styleUrls: ['./confirm.component.css']
 })
-export class ConfirmComponent {
+export class ConfirmComponent implements OnInit{
+
+  public reservationId : number = 0;
+
+  constructor(private activatedRoute : ActivatedRoute, ){}
+
+  ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id !== null){
+      this.reservationId = Number.parseInt(id);
+    }
+
+  }
 
 }
